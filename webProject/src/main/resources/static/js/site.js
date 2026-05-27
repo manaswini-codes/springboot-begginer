7a0abd5400aaaf83759a9d621b9936724cb71c7b
manaswini kommireddy <kommireddymanaswinireddy43@gmail.com>
2026-05-27 10:53:19 +0530

    my first commit

--- webProject/src/main/resources/static/js/site.js
+++ webProject/src/main/resources/static/js/site.js
@@ -1,5 +1,5 @@
+//alert("Hello from JS");
 console.log("Hello from JS");
-
 var users = [
     {"name" : "John", "gender" : "Male", "img" : "img/john.png"},
     {"name" : "Jane", "gender" : "Female", "img" : "img/jane.png"}
