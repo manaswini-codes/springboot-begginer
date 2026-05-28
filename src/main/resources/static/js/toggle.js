3a6c96aef8857976d82847c8d776d5f270839e26
manaswini kommireddy <kommireddymanaswinireddy43@gmail.com>
2026-05-28 17:44:50 +0530

    Implemented navbar and user toggle functionality

---
+++ src/main/resources/static/js/toggle.js
@@ -0,0 +1,33 @@
+var users = [
+    {
+        "name": "John",
+        "gender": "Male",
+        "img": "img/john.png"
+    },
+
+    {
+        "name": "Jane",
+        "gender": "Female",
+        "img": "img/jane.png"
+    }
+];
+
+var currentUserIndex = 0;
+
+function toggleCard() {
+
+    currentUserIndex = (currentUserIndex + 1) % 2;
+
+    var user = users[currentUserIndex];
+
+    displayUser(user);
+}
+
+function displayUser(user) {
+
+    document.getElementById("userImage").src = user.img;
+
+    document.getElementById("userGender").innerHTML = user.gender;
+
+    document.getElementById("userName").innerHTML = user.name;
+}
